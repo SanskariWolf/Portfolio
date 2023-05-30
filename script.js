f2ce91a387f9833bb2547de3bc72d4bf4a277c5c
SanskariWolf <90158167+SanskariWolf@users.noreply.github.com>
2023-05-30 09:38:53 +0530

    TIme pass

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // script.js
 
+// Add 'show' class to sections when they are scrolled into view
+function animateOnScroll() {
+    var sections = document.querySelectorAll('.section');
+
+    sections.forEach(function(section) {
+        if (isElementInViewport(section)) {
+            section.classList.add('show');
+        }
+    });
+}
+
+// Check if an element is in the viewport
+function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Scroll event listener
+window.addEventListener('scroll', animateOnScroll);
+
 // Form submission handler
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form from submitting
@@ -21,4 +46,14 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     document.getElementById('contactForm').appendChild(confirmation);
 
     // Reset form fields
-    document.getElementById
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+});
+
+// Trigger animations on page load
+document.addEventListener('DOMContentLoaded', function() {
+    animateOnScroll();
+
+    // Additional animations or actions can be added here
+});
